293e525119054f025bd5ff22b9c8195e07fced8d
AlekseiNikitsin <alekseevich129@gmail.com>
2023-11-10 20:33:13 +0300

    Prob

--- name/my_name.py
+++
@@ -1,6 +0,0 @@
-tc = [12.0,2131.0]
-tf = []
-for i in range(len(tc)-1):
-    tf.append((tc[i]*1.8+32)*10//1/10)
-    print(tc)
-    print(tf)
--- nova/python.py
+++
@@ -1,4 +0,0 @@
-import os
-import random
-os.mkdir("папка")
-print(random.randint(1,10),os.getcwd())
